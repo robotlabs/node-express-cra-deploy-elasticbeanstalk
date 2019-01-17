ba982bb3d21fbc4543da0e2d27ebaae0b106b625
Roberto Cascavilla <roberto.cascavilla@gmail.com>
2019-01-17 13:30:31 +0000

    to es6

--- app.js
+++ app.js
@@ -4,16 +4,14 @@ const app = express();
 var bodyParser = require('body-parser');
 
 app.get('/api/hello', (req, res) => {
-    res.send({ express: 'Hi, Ciao io sono Bellissimo: ' + Math.floor(Math.random() * 100) });
+    res.send({ express: 'Hi, Ciao bella: ' + Math.floor(Math.random() * 100) });
 });
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', function(req, res) {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 var port = process.env.PORT || 3000;
 
-var server = app.listen(port, function () {
-    console.log('Server running at http://127.0.0.1:' + port + '/');
-});
+app.listen(port, () => console.log(`Listening on port ${port}`));
